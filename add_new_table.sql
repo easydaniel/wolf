6935324e3b79bc33be0f1800ffbb83ee9ba50a98
easydaniel <whitetiger50512@gmail.com>
2024-06-16 11:33:17 -0700

    add new table

---
+++ add_new_table.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO public."role" ("name",id,"number",functionname,camp) VALUES 
+('狼術師 🪄',1017,0,NULL,NULL)
+INSERT INTO public."role" ("name",id,"number",functionname,camp) VALUES 
+('假面 🪄',1016,0,NULL,NULL)
+INSERT INTO public."role" ("name",id,"number",functionname,camp) VALUES 
+('舞者 🪄',17,0,NULL,NULL)
+INSERT INTO public.template_header ("name",description,isenabled) VALUES 
+('020. 假面舞會','',false);
+INSERT INTO public.template_header ("name",description,isenabled) VALUES 
+('021. 魔幻對決','',false);
+
+
+
+
+INSERT INTO public.template_role ("name", roleid, "number", darkpriority) VALUES 
+('021. 魔幻對決', 1, 1, 4)
+,('021. 魔幻對決', 11, 1, 0)
+,('021. 魔幻對決', 2, 1, 3)
+,('021. 魔幻對決', 3, 1, 5)
+,('021. 魔幻對決', 1001, 3, 2)
+,('021. 魔幻對決', 1017, 1, 1)
+,('021. 魔幻對決', 2001, 4, 99);
